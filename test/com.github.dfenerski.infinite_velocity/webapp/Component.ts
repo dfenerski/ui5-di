f34b671bd3f5d70a31158fccb64f8848fad5bbf1
Dimitar Fenerski <dfenerski@gmail.com>
2024-02-13 14:05:27 +0200

    refactor: format code

--- test/com.github.dfenerski.infinite_velocity/webapp/Component.ts
+++ test/com.github.dfenerski.infinite_velocity/webapp/Component.ts
@@ -1,37 +1,37 @@
-import UIComponent from "sap/ui/core/UIComponent";
-import { settle } from "ui5-di";
-import { AppStateFactory } from "./modules/app-state/AppState.factory";
+import UIComponent from 'sap/ui/core/UIComponent';
+import { settle } from 'ui5-di';
+import { AppStateFactory } from './modules/app-state/AppState.factory';
 import {
     APP_INSTANCE_1,
     APP_INSTANCE_2,
-} from "./modules/app-state/AppState.service";
+} from './modules/app-state/AppState.service';
 
 /**
  * @namespace com.github.dfenerski.infinite_velocity
  */
 export default class Component extends UIComponent {
     public static metadata = {
-        manifest: "json",
+        manifest: 'json',
     };
 
     public constructor() {
-        super("com.github.dfenerski.infinite_velocity.Component");
+        super('com.github.dfenerski.infinite_velocity.Component');
         //
         settle(APP_INSTANCE_1, () =>
             AppStateFactory.createAppStateService({
-                modelName: "app1",
+                modelName: 'app1',
                 data: {
-                    invoiceId: "INV-1",
+                    invoiceId: 'INV-1',
                 },
-            })
+            }),
         );
         settle(APP_INSTANCE_2, () =>
             AppStateFactory.createAppStateService({
-                modelName: "app2",
+                modelName: 'app2',
                 data: {
-                    invoiceId: "INV-2",
+                    invoiceId: 'INV-2',
                 },
-            })
+            }),
         );
     }
 
